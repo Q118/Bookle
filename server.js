300bf04d35559ca3ba0dcc59717c2b9b77502799
Q118 <shelbyfish91@gmail.com>
2020-08-15 17:45:38 -0700

    updated to use mLab in server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(routes);
 // Send every other request to the React app
 
 // connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookle-express");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user5:password5@ds127163.mlab.com:27163/heroku_fmxn8grg");
 
 // Define any API routes before this runs
 app.get("*", (req, res) => {
