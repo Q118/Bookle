957eeb21db809c71ed9b51846d7a510dd2959263
Q118 <shelbyfish91@gmail.com>
2020-08-15 18:22:27 -0700

    setting up routes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const mongoose = require("mongoose");
 
-// const routes = require("./routes");
+const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-// app.use(routes);
+app.use(routes);
 // Send every other request to the React app
 
 // connect to the Mongo DB
