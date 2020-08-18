cc934df738d210e4d82cf5aa590102e3c74dadf5
Q118 <shelbyfish91@gmail.com>
2020-08-17 19:51:27 -0700

    routes set up in nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,7 @@ function Nav() {
 	return (
 		<div className="myNav mb-3">
 			<nav className="navbar navbar-expand-lg p-3">
-				<a className="navbar-brand" href="#">
+				<a className="navbar-brand" href="/">
 					GoogleBooks
 				</a>
 				<button
@@ -23,12 +23,12 @@ function Nav() {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav mr-auto">
 						<li className="nav-item active">
-							<a className="nav-link" href="#">
+							<a className="nav-link" href="/search">
 								Search <span className="sr-only">(current)</span>
 							</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">
+							<a className="nav-link" href="/saved">
 								Saved <span className="sr-only">(current)</span>
 							</a>
 						</li>
